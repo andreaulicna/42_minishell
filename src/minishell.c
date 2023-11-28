f63f88003e2adcd31a46715cc1ba03ea6e3b4dfd
vbartos <vojtabar119@gmail.com>
2023-11-28 06:31:21 +0100

    test remote connection

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: vbartos <vbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 10:26:36 by vbartos           #+#    #+#             */
-/*   Updated: 2023/11/27 14:43:31 by vbartos          ###   ########.fr       */
+/*   Updated: 2023/11/28 06:30:52 by vbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,31 @@
 
 int main(int argc, char **argv, char **envp)
 {
-	// char	*prompt;
-	// char	*cmd;
-	// size_t	n;
+	char	*prompt;
+	char	*cmd;
+	size_t	n;
 
-	// prompt = PROMPT;
-	// cmd = NULL;
-	// n = 0;
-	// while (1)
-	// {
-	// 	printf("%s", prompt);
-	// 	getline(&cmd, &n, stdin);
-	// }
-	// free(cmd);
+	prompt = PROMPT;
+	cmd = NULL;
+	n = 0;
+	while (1)
+	{
+		printf("%s", prompt);
+		getline(&cmd, &n, stdin);
+		ft_cd(cmd);
+	}
+	free(cmd);
 
 	(void) argc;
 	(void) argv;
 
 	// char *echo_cmd[] = {"echo", "ahojda", "kamarade", NULL};
-	char *echo_cmd2[] = {"echo", "-n", "ahojda", "kamarade", NULL};
-	ft_echo(echo_cmd2);
-	ft_printf("\n\n\n");
-	ft_pwd();
-	ft_printf("\n\n\n");
-	ft_env(envp);
-	ft_printf("\n\n\n");
+	// char *echo_cmd2[] = {"echo", "-n", "ahojda", "kamarade", NULL};
+	// ft_echo(echo_cmd2);
+	// ft_printf("\n\n\n");
+	// ft_pwd();
+	// ft_printf("\n\n\n");
+	// ft_env(envp);
+	// ft_printf("\n\n\n");
 	return (0);
 }
