86593ee387a272ecc3054951a7ea30a0a2e268b9
vbartos <vojtabar119@gmail.com>
2023-12-05 17:59:09 +0100

    add free_data + minishell exit handler function

---
+++ main/free.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbartos <vbartos@student.42prague.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 14:27:08 by vbartos           #+#    #+#             */
+/*   Updated: 2023/12/05 17:39:47 by vbartos          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/minishell.h"
+
+// free_array
+// - frees a dynamically allocated 2D array;
+int	free_array(char **strs)
+{
+	int	i;
+
+	i = 0;
+	while (strs[i] != NULL)
+	{
+		free(strs[i]);
+		i++;
+	}
+	free(strs);
+	return (0);
+}
+
+void	free_linkedlist(t_list **head)
+{
+	t_list *current = *head;
+	t_list *next;
+
+	while (current != NULL) 
+	{
+		next = current->next;
+		free(current->content);
+		free(current);
+		current = next;
+	}
+	*head = NULL;
+}
+
+// free_env
+// - called as an argument to ft_lstclear;
+// - frees the content of the environment variables linked list;
+// void	free_env(void *ptr)
+// {
+// 	t_list	*env;
+
+// 	env = ptr;
+// 	if (env->content != NULL)
+// 		free(env->content);
+// 	free(env);
+// 	env = NULL;
+// }
+
+// free_data
+// - frees the entire data struct;
+void	free_data(t_data *data)
+{
+	if (data->env_list != NULL)
+		free_linkedlist(&data->env_list);
+}
--- main/utils.c
+++ main/utils.c
@@ -6,11 +6,30 @@
 /*   By: vbartos <vbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 11:48:52 by vbartos           #+#    #+#             */
-/*   Updated: 2023/11/29 13:23:28 by vbartos          ###   ########.fr       */
+/*   Updated: 2023/12/05 17:03:55 by vbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+// init_data
+// - initializes data struct;
+int		init_data(t_data *data, char **envp)
+{
+	env_init(envp, data);
+	//parser_init;
+	//lexer_init;
+	return (0);
+}
+
+// exit_minishell
+// - frees all allocated memory;
+// - exits with passed error;
+void	exit_minishell(t_data *data, int exit_status)
+{
+	free_data(data);
+	exit(exit_status);
+}
 
 // strs_count
 // - counts the amount of strings in a 2D array;
@@ -22,20 +41,4 @@ size_t	strs_count(char **strs)
 	while (strs[strs_count])
 		strs_count++;
 	return (strs_count);
-}
-
-// free_array
-// - frees a dynamically allocated 2D array;
-int		free_array(char **strs)
-{
-	int	i;
-
-	i = 0;
-	while (strs[i] != NULL)
-	{
-		free(strs[i]);
-		i++;
-	}
-	free(strs);
-	return (0);
 }
