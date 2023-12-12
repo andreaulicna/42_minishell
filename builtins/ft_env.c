1a9cbe72d94538865ace97441d0f6d6f421df922
vbartos <vojtabar119@gmail.com>
2023-12-12 10:36:25 +0100

    #57 change functionality to only print env variables with assigned values

--- builtins/ft_env.c
+++ builtins/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: vbartos <vbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 14:24:31 by vbartos           #+#    #+#             */
-/*   Updated: 2023/12/06 14:09:40 by vbartos          ###   ########.fr       */
+/*   Updated: 2023/12/12 10:26:30 by vbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 // ft_env
 // - prints the list of environment variables;
+// - only prints variables with assigned values;
 int	ft_env(t_data *data)
 {
 	t_list	*current;
@@ -23,9 +24,12 @@ int	ft_env(t_data *data)
 	while (current != NULL)
 	{
 		env = (t_env *)current->content;
-		ft_putstr_fd(env->name, STDOUT);
-		ft_putchar_fd('=', STDOUT);
-		ft_putendl_fd(env->value, STDOUT);
+		if (env->value != NULL)
+		{
+			ft_putstr_fd(env->name, STDOUT);
+			ft_putchar_fd('=', STDOUT);
+			ft_putendl_fd(env->value, STDOUT);
+		}
 		current = current->next;
 	}
 	return (0);
