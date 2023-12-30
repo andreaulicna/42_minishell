bbfa1ac0452f31b0430e0a121e04123fa6ec4c74
vbartos <vojtabar119@gmail.com>
2023-12-30 16:42:25 +0100

    version 2, also broken

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: vbartos <vbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 11:33:30 by vbartos           #+#    #+#             */
-/*   Updated: 2023/12/28 18:09:09 by vbartos          ###   ########.fr       */
+/*   Updated: 2023/12/30 16:08:39 by vbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	exec(t_data *data, t_list *simple_cmds)
 	if (cmd_num == 1)
 		exec_singlecmd(data, simple_cmds);
 	else if (cmd_num > 1)
-		exec_multicmds(data, simple_cmds, cmd_num);
+		exec_multicmds(data, simple_cmds);
 	return (0);
 }
 
@@ -66,70 +66,147 @@ void exec_singlecmd(t_data *data, t_list *simple_cmds)
 	}
 }
 
-void exec_multicmds(t_data *data, t_list *simple_cmds, size_t cmd_num)
+// void exec_multicmds(t_data *data, t_list *simple_cmds, size_t cmd_num)
+// {
+// 	int	tmpin = dup(STDIN);
+// 	int	tmpout = dup(STDOUT);
+// 	int	fdin = dup(tmpin);
+//     int ret;
+//     int fdout;
+// 	int cmd_num_int = (int)cmd_num;
+// 	int	i;
+// 	char			*path;
+// 	char			**env_copy;
+// 	t_simple_cmds	*content;
+
+// 	env_copy = exec_copyenv(data);
+// 	i = 0;
+// 	while (i < cmd_num_int)
+// 	{
+// 		content = (t_simple_cmds *) simple_cmds->content;
+// 		dup2(fdin, STDIN);
+// 		close(fdin);
+// 		if (i == (cmd_num_int - 1))
+// 		{
+// 			fdout = dup(tmpout);
+// 		}
+// 		else 
+// 		{
+// 			int fdpipe[2];
+// 			pipe(fdpipe);
+// 			fdout = fdpipe[1];
+// 			fdin = fdpipe[0];
+// 		}
+// 		// if (content->redirects)
+// 		// 	exec_handleredirect(content->redirects);
+// 		dup2(fdout, 1);
+// 		close(fdout);
+// 		if (exec_isbuiltin(content->cmd[0]))
+// 			exec_runbuiltin(data, content->cmd);
+// 		else
+// 		{
+// 			ret = fork();
+// 			if (ret == -1)
+// 			{
+// 				free(env_copy);
+// 				exit_current_prompt(NULL);
+// 			}
+// 			if (ret == 0)
+// 			{
+// 				path = exec_findpath(data, content->cmd[0]);
+// 				if (path != NULL)
+// 				{
+// 					execve(path, content->cmd, env_copy);
+// 					free(path);
+// 				}
+// 				free(env_copy);
+// 				exit_current_prompt(NULL);
+// 			}
+// 		}
+// 		i++;
+// 		simple_cmds = simple_cmds->next;
+// 	}
+// 	dup2(tmpin, STDIN);
+// 	dup2(tmpout, STDOUT);
+// 	close(tmpin);
+// 	close(tmpout);
+// 	waitpid(ret, NULL, 0);
+// }
+
+void exec_multicmds(t_data *data, t_list *simple_cmds)
 {
-	int	tmpin = dup(STDIN);
-	int	tmpout = dup(STDOUT);
-	int	fdin = dup(tmpin);
-    int ret;
-    int fdout;
-	int cmd_num_int = (int)cmd_num;
-	int	i;
-	char			*path;
-	char			**env_copy;
+	int				saved_in;
+	int				saved_out;
+	int				pipefd[2];
+	int				fd_in;
+	int				fd_out;
 	t_simple_cmds	*content;
 
-	env_copy = exec_copyenv(data);
-	i = 0;
-	while (i < cmd_num_int)
+	saved_in = dup(STDIN);
+	saved_out = dup(STDOUT);
+	fd_in = dup(saved_in);
+	while (simple_cmds)
 	{
 		content = (t_simple_cmds *) simple_cmds->content;
-		dup2(fdin, STDIN);
-		close(fdin);
-		// if (content->redirects)
-		// 	exec_handleredirect(content->redirects);
-		if (i == (cmd_num_int - 1))
+		if (content->redirects)
+			exec_handleredirect(content->redirects);
+		if (simple_cmds->next == NULL)
 		{
-			fdout = dup(tmpout);
+			if (content->redirects)
+				exec_handleredirect(content->redirects);
+			else
+				fd_out = dup(saved_out);
 		}
-		else 
-		{
-			int fdpipe[2];
-			pipe(fdpipe);
-			fdout = fdpipe[1];
-			fdin = fdpipe[0];
-		}
-		dup2(fdout, 1);
-		close(fdout);
-		if (exec_isbuiltin(content->cmd[0]))
-			exec_runbuiltin(data, content->cmd);
 		else
 		{
-			ret = fork();
-			if (ret == -1)
-			{
-				free(env_copy);
-				exit_current_prompt(NULL);
-			}
-			if (ret == 0)
-			{
-				path = exec_findpath(data, content->cmd[0]);
-				if (path != NULL)
-				{
-					// fprintf(stdout, "HERE\n");
-					execve(path, content->cmd, env_copy);
-					free(path);
-				}
-				free(env_copy);
-				exit_current_prompt(NULL);
-			}
+			// fprintf(stdout, "Here\n");
+			pipe(pipefd);
+			fd_in = pipefd[0];
+			fd_out = pipefd[1];
 		}
-		i++;
+		dup2(fd_in, STDIN);
+		close(fd_in);
+		dup2(fd_out, STDOUT);
+		close(fd_out);
+		exec_cmd(data, content);
 		simple_cmds = simple_cmds->next;
 	}
-	dup2(tmpin, STDIN);
-	dup2(tmpout, STDOUT);
-	close(tmpin);
-	close(tmpout);
-	waitpid(ret, NULL, 0);
+	dup2(saved_in, STDIN);
+	dup2(saved_out, STDOUT);
+	close(saved_in);
+	close(saved_out);
+	wait(NULL);
+}
+
+void exec_cmd(t_data *data, t_simple_cmds *content)
+{
+	char	**env_copy;
+	int		pid;
+	char	*path;
+
+	if (exec_isbuiltin(content->cmd[0]))
+		exec_runbuiltin(data, content->cmd);
+	else
+	{
+		env_copy = exec_copyenv(data);
+		pid = fork();
+		if (pid == -1)
+		{
+			free(env_copy);
+			exit_current_prompt(NULL);
+		}
+		if (pid == 0)
+		{
+			path = exec_findpath(data, content->cmd[0]);
+			if (path != NULL)
+			{
+				execve(path, content->cmd, env_copy);
+				free(path);
+			}
+			free(env_copy);
+			exit_current_prompt(NULL);
+		}
+		free(env_copy);
+		free(path);
+	}
 }
