9f8eb868fc4442a036b602b8eaf465a24183372c
Andrea Ulicna <aulicna@c3r7s3.42prague.com>
2024-01-11 19:43:14 +0100

    Moved prompt construction into the minishell_loop. Now using the clean minishell_loop.

--- src/debug/main_a.c
+++ src/debug/main_a.c
@@ -6,7 +6,7 @@
 /*   By: aulicna <aulicna@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/23 14:33:13 by aulicna           #+#    #+#             */
-/*   Updated: 2024/01/11 16:27:01 by aulicna          ###   ########.fr       */
+/*   Updated: 2024/01/11 19:42:06 by aulicna          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,27 @@ int	check_enter_space(char *input)
 	return (1);
 }
 
+int	minishell_loop(t_data *data)
+{
+	data->prompt = set_prompt(data->env_list);
+	data->input = readline((const char *)data->prompt);
+	if (!check_input_null(data->input))
+	{
+		printf("exit\n");
+		exit_minishell(NULL, 50);
+	}
+	if (!check_quotes(data->input) || !check_enter_space(data->input))
+		exit_current_prompt(data);
+	data->input_split = ft_split_minishell(data->input, ' ');
+	input_arr_to_lexer_list(data);
+	lexer_to_simple_cmds(&data->lexer, &data->simple_cmds);
+	expander(data);
+	heredoc(data);
+	print_simple_cmds(&data->simple_cmds);
+	exit_current_prompt(data);
+	return (1);
+}
+
 //int	minishell_loop(t_data *data)
 //{
 //	data->input = readline((const char *)data->prompt);
@@ -38,61 +59,41 @@ int	check_enter_space(char *input)
 //	}
 //	if (!check_quotes(data->input) || !check_enter_space(data->input))
 //		exit_current_prompt(data);
+//	/* Split S*/
 //	data->input_split = ft_split_minishell(data->input, ' ');
-//	input_arr_to_lexer_list(data->input_split, &data->lexer);
+//	printf("Minishell split output:\n");
+//	print_input_split(data->input_split);
+//	printf("----------------------\n");
+//	/* Split E */
+//	/* Lexer - Link list S */
+//	printf("Minishell lexer output:\n");
+//	input_arr_to_lexer_list(data);
+//	print_lexer(&data->lexer);
+//	printf("----------------------\n");
+//	/* Lexer - Link list E */
+//	/* Parser - Link list S */
+//	printf("Minishell parser output:\n");
+//	printf("----------------------\n");
 //	lexer_to_simple_cmds(&data->lexer, &data->simple_cmds);
-//	expander(data);
-//	heredoc(data);
+//	printf("Rest of lexer: \n");
+//	print_lexer(&data->lexer);
+//	printf("----------------------\n");
+//	printf("SIMPLE CMDS - before expander\n");
 //	print_simple_cmds(&data->simple_cmds);
+//	printf("----------------------\n");
+//	printf("SIMPLE CMDS - after expander\n");
+//	expander(data);
+//	print_simple_cmds(&data->simple_cmds);
+//	printf("----------------------\n");
+//	printf("Heredoc output:\n");
+//	heredoc(data);
+//	printf("----------------------\n");
+//	printf("SIMPLE CMDS - after heredoc\n");
+//	print_simple_cmds(&data->simple_cmds);
+//	/* Parser - Link list E */
 //	exit_current_prompt(data);
-//	return (1);
+//	return (1); //should never reach this
 //}
-
-int	minishell_loop(t_data *data)
-{
-	data->input = readline((const char *)data->prompt);
-	if (!check_input_null(data->input))
-	{
-		printf("exit\n");
-		exit_minishell(NULL, 50);
-	}
-	if (!check_quotes(data->input) || !check_enter_space(data->input))
-		exit_current_prompt(data);
-	/* Split S*/
-	data->input_split = ft_split_minishell(data->input, ' ');
-	printf("Minishell split output:\n");
-	print_input_split(data->input_split);
-	printf("----------------------\n");
-	/* Split E */
-	/* Lexer - Link list S */
-	printf("Minishell lexer output:\n");
-	input_arr_to_lexer_list(data);
-	print_lexer(&data->lexer);
-	printf("----------------------\n");
-	/* Lexer - Link list E */
-	/* Parser - Link list S */
-	printf("Minishell parser output:\n");
-	printf("----------------------\n");
-	lexer_to_simple_cmds(&data->lexer, &data->simple_cmds);
-	printf("Rest of lexer: \n");
-	print_lexer(&data->lexer);
-	printf("----------------------\n");
-	printf("SIMPLE CMDS - before expander\n");
-	print_simple_cmds(&data->simple_cmds);
-	printf("----------------------\n");
-	printf("SIMPLE CMDS - after expander\n");
-	expander(data);
-	print_simple_cmds(&data->simple_cmds);
-	printf("----------------------\n");
-	printf("Heredoc output:\n");
-	heredoc(data);
-	printf("----------------------\n");
-	printf("SIMPLE CMDS - after heredoc\n");
-	print_simple_cmds(&data->simple_cmds);
-	/* Parser - Link list E */
-	exit_current_prompt(data);
-	return (1); //should never reach this
-}
 
 int	main(int argc, char **argv, char *env[])
 {
@@ -106,7 +107,6 @@ int	main(int argc, char **argv, char *env[])
 	}
 	init_data(&data);
 	env_init(env, &data);
-	data.prompt = set_prompt(data.env_list);
 	minishell_loop(&data);
 	return (0);
 }
