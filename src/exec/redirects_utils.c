31a88d09a5adca3a48869c7bfb9ff46cd6261261
sl16 <vojtabar119@gmail.com>
2024-01-19 12:18:13 +0100

    update redirects_utils (still not working) + more testing files in gitignore

--- src/exec/redirects_utils.c
+++ src/exec/redirects_utils.c
@@ -6,80 +6,11 @@
 /*   By: vbartos <vbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/16 07:49:03 by vbartos           #+#    #+#             */
-/*   Updated: 2024/01/14 21:02:27 by vbartos          ###   ########.fr       */
+/*   Updated: 2024/01/19 10:12:06 by vbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/minishell.h"
-
-/**
- * @brief Handles the redirections specified in the given list.
- * 
- * This function iterates over the list of redirects and performs the correct
- * action based on the type of redirection. The supported redirection types are:
- * - 4: Output redirection (single '>' operator)
- * - 5: Output redirection (append '>>' operator)
- * - 2: Input redirection (single '<' operator)
- * - 3: Input redirection (heredoc '<<' operator)
- * 
- * @param redirects The list of redirects to handle.
- */
-void handle_redirect(t_data *data, t_list *redirects, char *hd_file)
-{
-	t_list	*curr_redirect = redirects;
-	t_lexer	*content;
-	int		redirection;
-	char	*filename;
-	
-	while (curr_redirect != NULL)
-	{
-		content = (t_lexer *)curr_redirect->content;
-		redirection = content->token;
-		filename = content->word;
-		if (redirection == GREATER)
-			handle_output_single(data, filename);
-		else if (redirection == GREATER_2)
-			handle_output_append(filename);
-		else if (redirection == LESS)
-			handle_input(data, filename);
-		else if (hd_file)
-			handle_input(data, hd_file);
-		curr_redirect = curr_redirect->next;
-	}
-}
-
-// void handle_redirect(t_list *redirects, char *hd_file)
-// {
-// 	t_list	*curr_redirect = redirects;
-// 	t_lexer	*content;
-// 	int		redirection;
-// 	char	*filename;
-	
-// 	while (curr_redirect != NULL)
-// 	{
-// 		content = (t_lexer *)curr_redirect->content;
-// 		redirection = content->token;
-// 		filename = content->word;
-// 		if (redirection == 4)
-// 		{
-// 			if (access(filename, W_OK) == 0)
-// 				handle_output_single(filename);
-// 		}
-// 		else if (redirection == 5)
-// 		{
-// 			if (access(filename, W_OK) == 0)
-// 				handle_output_append(filename);
-// 		}
-// 		else if (redirection == 2)
-// 		{
-// 			if (access(filename, R_OK) == 0)
-// 							handle_input(filename);
-// 		}
-// 		else if (hd_file)
-// 				handle_input(hd_file);
-// 		curr_redirect = curr_redirect->next;
-// 	}
-// }
 
 /**
  * @brief Handles redirecting standard output to a file, overwriting the file
@@ -134,12 +65,6 @@ void handle_input(t_data *data, char *filename)
 {
 	int fd;
 	
-	if (access(filename, R_OK) != 0)
-	{
-		ft_putendl_fd("minishell : Permission denied", STDERR);
-		data->exit_status = 1;
-		return ;
-	}
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 	{
@@ -147,14 +72,56 @@ void handle_input(t_data *data, char *filename)
 		ft_putstr_fd(filename, STDERR);
 		ft_putendl_fd(": No such file or directory", STDERR);
 		data->exit_status = 1;
-		return ;
+		exit_current_prompt(data);
 	}
 	if (fd > 0 && dup2(fd, STDIN) < 0)
 	{
 		ft_putendl_fd("minishell: pipe error", STDERR);
 		data->exit_status = 1;
-		return ;
+		exit_current_prompt(data);
+	}
+	if (access(filename, R_OK) == -1)
+	{
+		ft_putendl_fd("minishell: Permission denied", STDERR);
+		data->exit_status = 1;
+		exit_current_prompt(data);
 	}
 	if (fd > 0)
 		close(fd);
+}
+
+/**
+ * @brief Handles the redirections specified in the given list.
+ * 
+ * This function iterates over the list of redirects and performs the correct
+ * action based on the type of redirection. The supported redirection types are:
+ * - 4: Output redirection (single '>' operator)
+ * - 5: Output redirection (append '>>' operator)
+ * - 2: Input redirection (single '<' operator)
+ * - 3: Input redirection (heredoc '<<' operator)
+ * 
+ * @param redirects The list of redirects to handle.
+ */
+void handle_redirect(t_data *data, t_list *redirects, char *hd_file)
+{
+	t_list	*curr_redirect = redirects;
+	t_lexer	*content;
+	int		redirection;
+	char	*filename;
+	
+	while (curr_redirect != NULL)
+	{
+		content = (t_lexer *)curr_redirect->content;
+		redirection = content->token;
+		filename = content->word;
+		if (redirection == GREATER)
+			handle_output_single(data, filename);
+		else if (redirection == GREATER_2)
+			handle_output_append(filename);
+		else if (redirection == LESS)
+			handle_input(data, filename);
+		else if (hd_file)
+			handle_input(data, hd_file);
+		curr_redirect = curr_redirect->next;
+	}
 }
